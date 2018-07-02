ec1b61c82856e223723fd95d4846410d644a9478
Numfor Tiapo <numsmt2@gmail.com>
2018-07-02 00:29:41 -0400

    Making Title Page Responsive

--- test/controllers/main_controller_test.rb
+++ test/controllers/main_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class MainControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get main_index_url
-    assert_response :success
-  end
+  test "GET new" do
 
+    end
 end
