c6b7de2fffd37d3e92ec7c2db3d44f61b9d661f3
Numfor Tiapo <numsmt2@gmail.com>
2018-08-01 14:44:37 -0400

    Fixing Title background image for deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # To show background image on deployment
+  config.serve_static_assets = true
+  config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
