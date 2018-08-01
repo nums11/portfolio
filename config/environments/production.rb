3e3d4784defa0f5d310e6fb6bab6b3748a07c55c
Numfor Tiapo <numsmt2@gmail.com>
2018-08-01 14:51:22 -0400

    Compiling Assets Locally and changing to image-url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,3 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-
-  # To show background image on deployment
-  config.serve_static_assets = true
-  config.assets.compile = true
