75780c3be2ce41ff9632d5aa4dba065981a55f43
Numfor Tiapo <numsmt2@gmail.com>
2018-08-03 19:03:32 -0400

    Hardcoding Sendgrid API Key

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -42,7 +42,7 @@ class MainController < ApplicationController
   	# puts JSON.pretty_generate(mail.to_json)
   	puts mail.to_json
 	
-  	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+  	sg = SendGrid::API.new(api_key: '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', host: 'https://api.sendgrid.com')
   	response = sg.client.mail._('send').post(request_body: mail.to_json)
   	puts response.status_code
   	puts response.body
