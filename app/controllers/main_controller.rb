11528aaea11308b9df777eae0e9ded4356b79a74
Numfor Tiapo <numsmt2@gmail.com>
2018-08-03 18:58:25 -0400

    Testgin SendGrid

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,52 @@
+require 'sendgrid-ruby'
+include SendGrid
+
 class MainController < ApplicationController
   def index
+  end
+
+  def email
+	# data = JSON.parse('{
+	#   "personalizations": [
+	#     {
+	#       "to": [
+	#         {
+	#           "email": "numsmt2@gmail.com"
+	#         }
+	#       ],
+	#       "subject": "EMAIL FROM WEBSITE"
+	#     }
+	#   ],
+	#   "from": {
+	#     "email": "test@example.com"
+	#   },
+	#   "content": [
+	#     {
+	#       "type": "text/plain",
+	#       "value": "This is an email from your website."
+	#     }
+	#   ]
+	# }')
+	# sg = SendGrid::API.new(api_key: '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')
+	# response = sg.client.mail._("send").post(request_body: data)
+	# puts response.status_code
+	# puts response.body
+	# # puts response.parsed_body
+	# puts response.headers
+
+	from = Email.new(email: 'test@example.com')
+  	subject = 'WEBSITE MESSAGE'
+  	to = Email.new(email: 'numsmt2@gmail.com')
+  	content = Content.new(type: 'text/plain', value: 'Message From Website!')
+  	mail = Mail.new(from, subject, to, content)
+  	# puts JSON.pretty_generate(mail.to_json)
+  	puts mail.to_json
+	
+  	sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'], host: 'https://api.sendgrid.com')
+  	response = sg.client.mail._('send').post(request_body: mail.to_json)
+  	puts response.status_code
+  	puts response.body
+  	puts response.headers
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "main#index"
+  get 'email', to: 'main#email'
   get 'contact-me', to: 'messages#new', as: 'new_message'
   post 'contact-me', to: 'messages#create', as: 'create_message'
 end
