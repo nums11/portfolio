ecc594ca0cf8a43834c275ae7f70b3112bf37401
Numfor Tiapo <numsmt2@gmail.com>
2018-08-03 20:39:10 -0400

    Changing SendGrid Settings

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -36,8 +36,8 @@ class MainController < ApplicationController
 	from = SendGrid::Email.new(email: 'test@example.com')
   	subject = 'WEBSITE MESSAGE'
   	to = SendGrid::Email.new(email: 'numsmt2@gmail.com')
-  	content = SendGrid::Content.new(type: 'text/plain', value: 'Message From Website!')
-  	mail = SendGrid::Mail.new(from, subject, to, content)
+  	content = Content.new(type: 'text/plain', value: 'Message From Website!')
+  	mail = Mail.new(from, subject, to, content)
   	# puts JSON.pretty_generate(mail.to_json)
   	puts mail.to_json
 	
