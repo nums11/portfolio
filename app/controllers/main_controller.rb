030d8d9e0fac68c90cc991c5ec85ac8904a1b307
Numfor Tiapo <numsmt2@gmail.com>
2018-08-12 18:41:23 -0400

    Removing SendGrid API Key from visible code

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -6,34 +6,6 @@ class MainController < ApplicationController
   end
 
   def email
-	# data = JSON.parse('{
-	#   "personalizations": [
-	#     {
-	#       "to": [
-	#         {
-	#           "email": "numsmt2@gmail.com"
-	#         }
-	#       ],
-	#       "subject": "EMAIL FROM WEBSITE"
-	#     }
-	#   ],
-	#   "from": {
-	#     "email": "test@example.com"
-	#   },
-	#   "content": [
-	#     {
-	#       "type": "text/plain",
-	#       "value": "This is an email from your website."
-	#     }
-	#   ]
-	# }')
-	# sg = SendGrid::API.new(api_key: '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')
-	# response = sg.client.mail._("send").post(request_body: data)
-	# puts response.status_code
-	# puts response.body
-	# # puts response.parsed_body
-	# puts response.headers
-
 	from = SendGrid::Email.new(email: 'test@example.com')
   	subject = 'WEBSITE MESSAGE'
   	to = SendGrid::Email.new(email: 'numsmt2@gmail.com')
